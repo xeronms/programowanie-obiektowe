fd3deae83c64f0c52d424deea7829853ed317a40
Xeron <xeronms@o2.pl>
2018-10-16 22:05:33 +0200

    loading done

--- 2/zad2/Test.java
+++ 2/zad2/Test.java
@@ -1,19 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Test{
 
 
+	private static LinkedList < Rectangle > shapes ;
 
-public class main{
+
+	private static void loadRect(){
+
+	    System.out.print("enter rectangle's sides' length");
+
+        try {
+
+            BufferedReader reader = new BufferedReader( new InputStreamReader(System.in) );
+
+            double a = Double.parseDouble(reader.readLine());
+
+            double b = Double.parseDouble(reader.readLine());
+
+            shapes.add( new Rectangle( a , b ));
+
+
+        }catch (IOException e){}
+
+	}
+
 
 	public static void main( String [] args ){
 	
 	
-		Square S = new Square (5);
+		int i = 1;
 
-		Rectangle R = new Rectangle( 10, 5);
+		while (i != 0) {
 
-		System.out.print(S.area() + R.area());
+			try {
+
+				InputStreamReader reader = new InputStreamReader(System.in);
+
+				BufferedReader buffer = new BufferedReader(reader);
+
+				i = Integer.parseInt(buffer.readLine());
 
 
-	
+				System.out.print(i);
+
+				switch (i){
+					case 1:
+						//
+                        loadRect();
+						break;
+
+					case 2:
+						//
+						break;
+
+					case 0:
+						break;
+
+					default:
+						//
+						break;
+
+				}
+
+			}catch (IOException e){}
+
+		}
 	
 	}
 
