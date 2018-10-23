1af061aae5f17213e5b84c2d7ced30f460d0c5ed
Xeron <xeronms@o2.pl>
2018-10-23 23:17:14 +0200

    mail validation

--- 3/src/Main.java
+++ 3/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main( String [] args ){
 
-        EmailMessage mail = EmailMessage.builder().addFrom("SD").build();
+        EmailMessage mail = EmailMessage.builder().addFrom("SD.SD@asd.sd").build();
         System.out.print(mail.get( ));
     }
 
--- 3/src/mail/EmailMessage.java
+++ 3/src/mail/EmailMessage.java
@@ -1,5 +1,6 @@
 package mail;
 import java.util.LinkedList;
+import java.util.regex.Pattern;
 
 public class EmailMessage{
 
@@ -27,7 +28,8 @@ public class EmailMessage{
     }
 
 
-    // Builder class
+    //
+    //Builder class
     public static class Builder{
 
         static EmailMessage m;
@@ -37,18 +39,37 @@ public class EmailMessage{
         }
 
         public Builder addFrom( String from ){
-            m.from = from;
+            try {
+                Checker.checkMail(from);
+            } catch (Exception e){ System.out.print("EMAIL INVALID");}
+
+            finally {
+                m.from = from;
+            }
+
             return this;
         }
 
-        public static EmailMessage build(){
+        public EmailMessage build(){
             return new EmailMessage( m );
         }
 
     }
 
 
+    //
+    //Email address check
+    private static class Checker{
 
+        private static void checkMail (String mail ) throws Exception{
+            Pattern p = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+            // Throwing exception in case email is not valid
+            if ( ! p.matcher( mail ).matches() )
+                throw new Exception("WRONG EMAIL GIVEN");
+
+        }
+    }
 
 
 
