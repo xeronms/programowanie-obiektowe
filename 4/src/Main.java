16924ea26bbb04a7d2cd8e7ab66d5706f3dbecc0
unknown <xeronms@gmail.com>
2018-11-01 23:58:31 +0100

    finish

--- 4/src/Main.java
+++ 4/src/Main.java
@@ -4,6 +4,7 @@ import cipher.ROT11;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
@@ -14,8 +15,32 @@ public class Main {
         ROT11 cipher = new ROT11();
         Polibius cipher2 = new Polibius();
 
+        char c1='4',c2;
+        Scanner scanner = new Scanner(System.in);
+
         try {
-            Cryptographer.decryptfile(in, out, cipher2);
+            System.out.print(" Choose to:\n" +
+                    "1. Crypt\n" +
+                    "2. Decrypt\n");
+            c1 = scanner.nextLine().charAt(0);
+
+            System.out.print(" Choose algorithm:\n" +
+                    "1. ROT11\n" +
+                    "2. Polibius\n");
+            c2 = scanner.nextLine().charAt(0);
+
+            if ( c1 == '1' && c2 == '1'){
+                Cryptographer.cryptfile(in,out,cipher);
+                System.out.println("done\n");}
+            if ( c1 == '1' && c2 == '2'){
+                Cryptographer.cryptfile(in,out,cipher2);
+                System.out.println("done\n");}
+            if ( c1 == '2' && c2 == '1'){
+                Cryptographer.decryptfile(in,out,cipher);
+                System.out.println("done\n");}
+            if ( c1 == '2' && c2 == '2'){
+                Cryptographer.decryptfile(in,out,cipher2);
+                System.out.println("done\n");}
         }
         catch (IOException e){System.out.print("INVALID PATH");}
 
