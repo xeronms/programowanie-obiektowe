666565385a22576f732fc6213dd26de66b7e9e05
xeronms <xeronms@o2.pl>
2018-11-08 23:03:18 +0100

    Numbers[5] version

--- 5/src/Subtitles.java
+++ 5/src/Subtitles.java
@@ -13,25 +13,48 @@ public class Subtitles {
             throws CharacterSequenceException, PatternMatchingException, IOException {
 
         List<String> lines = Files.readAllLines(in.toPath());
-        //BufferedWriter writer = new BufferedWriter(new FileWriter(out));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(out));
         StringBuilder sb = new StringBuilder();
+
+        int start,end, change;
         int i=0;
 
         String pattern = "^\\{\\d+}\\{\\d+}.*";
 
         for ( String line : lines ){
+            String Numbers[] = new String[5];
             ++i;
 
             if ( ! line.matches(pattern))
                 throw new PatternMatchingException(i);
 
-            // here i check if there is not such situation as {2323}{11}, where 2323>11
-            String Numbers[] = line.split("}", 3);
+            Numbers = line.split("}",5);
             Numbers[0] = Numbers[0].replaceAll("\\D+","");
             Numbers[1] = Numbers[1].replaceAll("\\D+","");
+            start = Integer.parseInt( Numbers[0]);
+            end = Integer.parseInt( Numbers[1]);
 
-            if ( Integer.parseInt( Numbers[0]) >= Integer.parseInt( Numbers[1]) )
+            if ( start >= end )
                 throw new CharacterSequenceException(i);
+
+            change = delay*fps;
+
+            start += change;
+            end += change;
+
+            sb.append("{");
+            sb.append(start);
+            sb.append("}{");
+            sb.append(end);
+
+            for ( int j=2; j<Numbers.length; ++j ) {
+                sb.append("}");
+                sb.append( Numbers[j] );
+                sb.append("\n");
+            }
+            writer.write( sb.toString() );
         }
+
+        writer.close();
     }
 }
