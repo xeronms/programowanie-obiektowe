f844fdfb3aa6a6b6770f80d4b91ba7161fe5ec14
Xeron <xeronms@o2.pl>
2018-11-23 01:32:07 +0100

    first

---
+++ 7/Polynomials/src/sample/Polynomial.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class Polynomial {
+
+    private double [] factors;
+
+    Polynomial(double [] factors){
+        this.factors = factors;
+    }
+
+    public double value(int x){
+        double v = 0;
+
+        for ( int i = 0 ; i < factors.length; ++i ){
+            v += factors[i] * Math.pow( x , i );
+            System.out.println(v);
+        }
+
+        return v;
+    }
+
+}
