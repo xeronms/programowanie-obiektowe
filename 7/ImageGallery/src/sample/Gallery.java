fd04ae5b7fb3c413ef10285ab748c18daa5e1d6b
Xeron <xeronms@o2.pl>
2018-11-25 10:29:32 +0100

    full gallery

--- 7/ImageGallery/src/sample/Gallery.java
+++ 7/ImageGallery/src/sample/Gallery.java
@@ -31,7 +31,7 @@ public class Gallery {
         // Path
         TextField pathField = new TextField();
         pathField.setPromptText( "Enter gallery path" );
-        pathField.setMaxSize(500,50);
+        pathField.setPrefSize(500, 20);
 
         Button button = new Button("Load");
         button.setOnAction(e -> {
@@ -42,12 +42,20 @@ public class Gallery {
 
         scroll.setContent(tile);
 
+        // ??
+        scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER); // Horizontal
+        scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED); // Vertical scroll bar
+        scroll.setFitToWidth(true);
+        scroll.setContent(tile);
+
         return scroll;
     }
 
     private static void fileLoad( TextField pathField ){
+        final String path;
         if ( pathField.getText()!=null && !pathField.getText().isEmpty()){
-            final String path = pathField.toString();
+            path = pathField.getText();
+            System.out.println(path);
 
             File file = new File(path);
             File [] photos = file.listFiles();
@@ -56,9 +64,12 @@ public class Gallery {
                 for (File photo : photos) {
 
                     ImageView imageView = createImage(photo);
+                    tile.getChildren().addAll(imageView);
+
                 }
             }
             catch ( NullPointerException|FileNotFoundException e){
+                System.out.println(e.getMessage());
                 e.fillInStackTrace();
             }
         }
@@ -67,36 +78,34 @@ public class Gallery {
     private static ImageView createImage( File file ) throws FileNotFoundException {
 
         final Image image = new Image(new FileInputStream( file ), 200, 0, true, true);
-        ImageView imageView = null;
-        imageView = new javafx.scene.image.ImageView(image);
+        ImageView imageView = new javafx.scene.image.ImageView(image);
         imageView.setFitWidth(200);
-        imageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                try {
-                    if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
-                        BorderPane borderPane = new BorderPane();
-                        ImageView imageView = new ImageView();
-                        Image image = new Image(new FileInputStream(file));
-                        imageView.setImage(image);
-                        imageView.setStyle("-fx-background-color: BLACK");
-                        imageView.setFitHeight(600);
-                        imageView.setPreserveRatio(true);
-                        imageView.setSmooth(true);
-                        imageView.setCache(true);
-                        borderPane.setCenter(imageView);
-                        borderPane.setStyle("-fx-background-color: BLACK");
-                        Stage newStage = new Stage();
-                        newStage.setWidth(600);
-                        newStage.setHeight(600);
-                        newStage.setTitle(file.getName());
-                        Scene scene = new Scene(borderPane, Color.BLACK);
-                        newStage.setScene(scene);
-                        newStage.show();
-                    }
-                }catch (FileNotFoundException e){
-                    e.printStackTrace();
+        imageView.setOnMouseClicked(mouseEvent -> {
+            try {
+                if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+                    BorderPane borderPane = new BorderPane();
+                    ImageView imageView1 = new ImageView();
+
+                    Image image1 = new Image(new FileInputStream(file));
+                    imageView1.setImage(image1);
+                    imageView1.setFitHeight(800);
+                    imageView1.setPreserveRatio(true);
+                    imageView1.setSmooth(true);
+                    imageView1.setCache(true);
+
+                    borderPane.setCenter(imageView1);
+
+                    Stage newStage = new Stage();
+                    newStage.setWidth(600);
+                    newStage.setHeight(800);
+                    newStage.setTitle(file.getName());
+
+                    Scene scene = new Scene(borderPane);
+                    newStage.setScene(scene);
+                    newStage.show();
                 }
+            }catch (FileNotFoundException e){
+                e.printStackTrace();
             }
         });
 
